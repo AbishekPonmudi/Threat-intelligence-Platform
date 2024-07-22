1427d34760e6399391bb5d9989fa8542acfd1a23
Havox <135835548+AbishekPonmudi@users.noreply.github.com>
2024-07-22 15:36:13 +0530

    Upgraded Running_services.py code

--- Network_code/Running_services.py
+++ Network_code/Running_services.py
@@ -2,164 +2,126 @@
 Copyrights(2024)@ Under MIT LICENSE
 Author = Havox """
 
-from scapy.all import *
-import psutil
-from collections import defaultdict
 import os
+import psutil
+import time
 import sys
 import ctypes
+import datetime
+from collections import defaultdict
 from threading import Thread
 import pandas as pd
-# get the all network adapter's MAC addresses
-all_macs = {iface.mac for iface in ifaces.values()}
-# A dictionary to map each connection to its correponding process ID (PID)
-connection2pid = {}
-# A dictionary to map each process ID (PID) to total Upload (0) and Download (1) traffic
-pid2traffic = defaultdict(lambda: [0, 0])
-# the global Pandas DataFrame that's used to track previous traffic stats
-global_df = None
-# global boolean for status of the program
-is_program_running = True
+from scapy.all import *
+
+# To collect the MAC addresses using iface by scapy
+mac_cap = {iface.mac for iface in ifaces.values()}
+connectionpid = {}  # Declaring an empty dictionary for mapping
+pidtraffic = defaultdict(lambda: [0, 0])  # Using lambda for upload and download data
+running = True
+
+# Declaring the bytes and types
+def byte_size(bytes):
+    for units in ["", "K", "M", "G", "T"]:
+        if bytes < 1024:
+            return f"{bytes:.2f}{units}B"
+        bytes /= 1024
 
 def running_as_admin():
     try:
         return ctypes.windll.shell32.IsUserAnAdmin()
     except:
         return False
+
 def run_as_admin():
     try:
-        code= sys.argv[0]
-        params_shellWin32 = ''.join([code] + sys.argv[1:])
-        subprocess.run(['powershell','start-process','python',f'-ArgumentList "{params_shellWin32}"','-Verb','runAs'],check=True) 
+        code = sys.argv[0]  # Get the file name
+        shellcode = ''.join([code] + sys.argv[1:])
+        subprocess.run(['powershell', 'start-process', 'python', '-ArgumentList', f"{shellcode}", '-verb', 'runas'], check=True)
     except:
         sys.exit(1)
-def get_size(bytes):
-    """
-    Returns size of bytes in a nice in all format like using kilo , giga , mega , tera
-    """
-    for unit in ["","k","G","M","T"]:
-        if bytes < 1024:
-            return f"{bytes:.2f}{unit}B"
-        bytes /= 1024
 
 def process_packet(packet):
-    global pid2traffic
     try:
-        # get the packet source & destination IP addresses and ports
         packet_connection = (packet.sport, packet.dport)
     except (AttributeError, IndexError):
-        # sometimes the packet does not have TCP/UDP layers, we just ignore these packets
+        print("Some error occurred")
         pass
     else:
-        # get the PID responsible for this connection from our `connection2pid` global dictionary
-        packet_pid = connection2pid.get(packet_connection)
+        packet_pid = connectionpid.get(packet_connection)
         if packet_pid:
-            if packet.src in all_macs:
-                # the source MAC address of the packet is our MAC address
-                # so it's an outgoing packet, meaning it's upload
-                pid2traffic[packet_pid][0] += len(packet)
+            if packet.src in mac_cap:
+                pidtraffic[packet_pid][0] += len(packet)
             else:
-                # incoming packet, download
-                pid2traffic[packet_pid][1] += len(packet)
-def get_connections():
-    """A function that keeps listening for connections on this machine 
-    and adds them to `connection2pid` global variable"""
-    global connection2pid
-    while is_program_running:
-        # using psutil, we can grab each connection's source and destination ports
-        # and their process ID
+                pidtraffic[packet_pid][1] += len(packet)
+
+def get_connection():
+    while running:
         for c in psutil.net_connections():
-            if c.laddr and c.raddr and c.pid:
-                # if local address, remote address and PID are in the connection
-                # add them to our global dictionary
-                connection2pid[(c.laddr.port, c.raddr.port)] = c.pid
-                connection2pid[(c.raddr.port, c.laddr.port)] = c.pid
-        # sleep for a second, feel free to adjust this
+            if c.laddr and c.raddr:
+                connectionpid[(c.laddr.port, c.raddr.port)] = c.pid
+                connectionpid[(c.raddr.port, c.laddr.port)] = c.pid
         time.sleep(1)
 
-def print_pid2traffic():
+def print_pidtraffic():
     global global_df
-    # initialize the list of processes
     processes = []
-    for pid, traffic in pid2traffic.items():
-        # `pid` is an integer that represents the process ID
-        # `traffic` is a list of two values: total Upload and Download size in bytes
+    for pid, traffic in pidtraffic.items():
         try:
-            # get the process object from psutil
-            p = psutil.Process(pid)
+            p2id = psutil.Process(pid)
         except psutil.NoSuchProcess:
-            # if process is not found, simply continue to the next PID for now
             continue
-        # get the name of the process, such as chrome.exe, etc.
-        name = p.name()
-        # get the time the process was spawned
+        name = p2id.name()
+        
         try:
-            create_time = datetime.fromtimestamp(p.create_time())
+            create_time = datetime.fromtimestamp(p2id.create_time())
         except OSError:
-            # system processes, using boot time instead
             create_time = datetime.fromtimestamp(psutil.boot_time())
-        # construct our dictionary that stores process info
+            
         process = {
-            "pid": pid, "name": name, "create_time": create_time, "Upload": traffic[0],
-            "Download": traffic[1],
+            "Pid": pid, "Name": name, "Created_time": create_time,
+            "Upload": traffic[0], "Download": traffic[1]
         }
         try:
-            # calculate the upload and download speeds by simply subtracting the old stats from the new stats
-            process["Upload_Speed"] = traffic[0] - global_df.at[pid, "Upload"]
-            process["Download_Speed"] = traffic[1] - global_df.at[pid, "Download"]
+            process["Upload_speed"] = traffic[0] - global_df.at[pid, "Upload"]
+            process["Download_speed"] = traffic[1] - global_df.at[pid, "Download"]
         except (KeyError, AttributeError):
-            # If it's the first time running this function, then the speed is the current traffic
-            # You can think of it as if old traffic is 0
-            process["Upload_Speed"] = traffic[0]
-            process["Download_Speed"] = traffic[1]
-        # append the process to our processes list
-        processes.append(process)
-    # construct our Pandas DataFrame
+            process["Upload_speed"] = traffic[0]
+            process["Download_speed"] = traffic[1]
+            
+        processes.append(process)  
+    
     df = pd.DataFrame(processes)
     try:
-        # set the PID as the index of the dataframe
-        df = df.set_index("pid")
-        # sort by column, feel free to edit this column
+        df = df.set_index("Pid")
         df.sort_values("Download", inplace=True, ascending=False)
     except KeyError as e:
-        # when dataframe is empty
         pass
-    # make another copy of the dataframe just for fancy printing
+
     printing_df = df.copy()
     try:
-        # apply the function get_size to scale the stats like '532.6KB/s', etc.
-        printing_df["Download"] = printing_df["Download"].apply(get_size)
-        printing_df["Upload"] = printing_df["Upload"].apply(get_size)
-        printing_df["Download_Speed"] = printing_df["Download_Speed"].apply(get_size).apply(lambda s: f"{s}/s")
-        printing_df["Upload_Speed"] = printing_df["Upload_Speed"].apply(get_size).apply(lambda s: f"{s}/s")
+        printing_df["Download"] = printing_df["Download"].apply(byte_size)
+        printing_df["Upload"] = printing_df["Upload"].apply(byte_size)
+        printing_df["Download_speed"] = printing_df["Download_speed"].apply(byte_size).apply(lambda s: f"{s}/s")
+        printing_df["Upload_speed"] = printing_df["Upload_speed"].apply(byte_size).apply(lambda s: f"{s}/s")
     except KeyError as e:
-        # when dataframe is empty again
         pass
-    # clear the screen based on your OS
-    os.system("cls") if "nt" in os.name else os.system("clear")
-    # print our dataframe
+
+    os.system('cls' if os.name == 'nt' else 'clear')
     print(printing_df.to_string())
-    # update the global df to our dataframe
     global_df = df
 
 def print_stats():
-    """Simple function that keeps printing the stats"""
-    while is_program_running:
+    while running:
         time.sleep(1)
-        print_pid2traffic()
+        print_pidtraffic()
 
 if __name__ == "__main__":
-    # start the printing thread
     printing_thread = Thread(target=print_stats)
     printing_thread.start()
-    # start the get_connections() function to update the current connections of this machine
-    connections_thread = Thread(target=get_connections)
+   
+    connections_thread = Thread(target=get_connection)
     connections_thread.start()
 
- # start sniffing
     print("Started sniffing")
     sniff(prn=process_packet, store=False)
-    # run_as_admin()
-    # setting the global variable to False to exit the program
-    is_program_running = False   
-    # sys.exit(0)
+    running = False   
